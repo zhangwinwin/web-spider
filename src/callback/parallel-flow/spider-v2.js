16890ae8c5d2fa09ee1217166df1b1dfb04d1a2d
zhangwinwin <344879340@qq.com>
2022-06-26 21:39:48 +0800

    feat: 增加callback模式的第二个版本

---
+++ src/callback/parallel-flow/spider-v2.js
@@ -0,0 +1,81 @@
+import fs, { link } from 'fs'
+import path from 'path'
+import superagent from 'superagent'
+import mkdirp from 'mkdirp'
+import { urlToFilename, getPageLinks } from './utils.js'
+
+function saveFile (filename, contents, cb) {
+    mkdirp(path.dirname(filename), err => {
+        if (err) {
+            return cb(err)
+            }
+            fs.writeFile(filename, contents, cb)
+        })
+    }
+
+function download (url, filename, cb) {
+    console.log(`Downloading ${url}`)
+    superagent.get(url).end((err, res) => {
+        if (err) {
+            return cb(err)
+        }
+        saveFile(filename, res.text, err => {
+            if (err) {
+                return cb(err)
+            }
+            console.log(`Downloaded and saved: ${url}`)
+            cb(null, res.text)
+        })
+    })
+}
+
+function spiderLink (url, content, nesting, cb) {
+    if (nesting === 0)
+        return process.nextTick(cb)
+    const links = getPageLinks(url, content)
+    if (links.length === 0)
+        return process.nextTick(cb)
+    
+    let concurrency = 2
+    let completed = 0
+    let running = 0
+    let index = 0
+
+    function next () {
+        while (running < concurrency && index < links.length) {
+            const link = links[index++]
+            spider(link, nesting - 1, (err) => {
+                if (err) {
+                    hasErrors = true
+                    return cb(err)
+                }
+                if (++completed === links.length && !hasError)
+                    return cb()
+                running++;
+                next()
+            })
+        }
+        next()
+    }
+}
+
+const spidering = new Set();
+export function spider (url, nesting, cb) {
+    if(spidering.has(url))
+        return process.nextTick(cb)
+    spidering.add(url);
+    const filename = urlToFilename(url);
+    fs.readFile(filename, 'utf8', (err, fileContent) => {
+        if (err) {
+            if (err.code === 'ENOENT')
+                return cb(err)
+            return download(url, filename, (err, requestContent) => {
+                if (err)
+                    return cb(err)
+                
+                spiderLink(url, requestContent, nesting, cb)
+            })
+        }
+        spiderLink(url, fileContent, nesting, cb)
+    })
+}
