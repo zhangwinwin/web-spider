26fa2ca0a4dd2ab82b4abf4be723180d5909bc39
zhangwinwin <344879340@qq.com>
2022-07-02 09:47:31 +0800

    feat：增加promise模式的，队列

---
+++ src/promise/parallel-flow-v2/spider.js
@@ -0,0 +1,55 @@
+import { promises as fsPromises } from 'fs'
+import { dirname } from 'path'
+import superagent from 'superagent'
+import mkdirp from 'mkdirp'
+import { urlToFilename, getPageLinks } from './utils.js'
+import { promisify } from 'util'
+import { TaskQueue } from './TaskQueue.js'
+
+const mkdirpPromises = promisify(mkdirp)
+
+function download (url, filename) {
+    console.log(`Downloading ${url}`)
+    let content
+    return superagent.get(url)
+        .then(res => {
+            content = res.text
+            return mkdirpPromises(dirname(filename))
+        })
+        .then(() => fsPromises.writeFile(filename, content))
+        .then(() => {
+            console.log(`Downloaded and saved: ${url}`)
+            return content
+        })
+}
+
+function spiderLinks (url, content, nesting, queue) {
+    if (nesting === 0)
+        return Promise.resolve()
+    const links =  getPageLinks(url, content)
+    const promises = links.map(link => splider(link, nesting - 1, queue))
+    return Promise.all(promises)
+}
+
+const spidering = new Set()
+function spiderTask(url, nesting, queue) {
+    if (spidering.has(url))
+        return Promise.resolve()
+    spidering.add(url)
+
+    const filename = urlToFilename(url)
+    return queue.runTask(() => {
+        return fsPromises.readFile(filename, 'utf8')
+        .catch(err => {
+            if (err.code !== 'ENOENT')
+                throw err
+            return download(url, filename)
+        })
+        .then(content => spiderLinks(url, content, nesting, queue))
+    })
+}
+
+export function spider (url, nesting, concurrency) {
+    const queue = new TaskQueue(concurrency)
+    return spiderTask(url, nesting, queue)
+}
